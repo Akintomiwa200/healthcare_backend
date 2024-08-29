ce90df3f3a207e2eaa6b69205ab9c4771350d8d0
Akintomiwa Peter Adedokun <143067882+Akintomiwa200@users.noreply.github.com>
2024-08-29 15:59:58 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const app = express();
 
 
 const corsOptions = {
-  origin: "http://localhost:5173", 
+  origin: *, 
   methods: "GET, POST, PUT, DELETE, OPTIONS",
   allowedHeaders: ["Content-Type", "Authorization"], 
   credentials: true,
