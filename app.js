13d8dbc00b6d70f15b17b0c0359783087d56f4f8
Akintomiwa Peter Adedokun <143067882+Akintomiwa200@users.noreply.github.com>
2024-08-29 16:14:28 +0100

    Update app.js

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ const corsOptions = {
 
 app.use(cors(corsOptions)); 
 
-app.options(https://healthcare-backend-tdsu.onrender.com/api/auth/signup, cors(corsOptions)); 
+app.options("*", cors(corsOptions)); 
 
 
 app.use(express.json());
